9df4812685c7da3bdd5a8e345d8ebc97e7b02165
Guoyao Li <1986876893@qq.com>
2023-03-08 11:08:53 -0500

    photo added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,71 +1,80 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Aboutcard from "./BioCard";
-
 import Particle from "../Particle";
 import Type from "./Type";
-
-
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import guoyaoLiImg from "../../Assets/photo.JPG";
 
 function Home() {
   return (
     <section>
-
       <Container fluid className="about-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          <strong className="purple">Guoyao Li</strong>
-        </h1>
-        <Aboutcard />
+        <Particle />
+        <Container>
+          <Row>
+            <Col md={5} className="about-img">
+              <img
+                src={guoyaoLiImg}
+                alt="Guoyao Li"
+                className="img-fluid"
+                style={{ alignSelf: "right", marginBottom: "10px", transform: "scale(1)", }}
+              />
+            </Col>
+            <Col>
+              <h1 className="project-heading"
+                style={{
+                  position: "absolute",
+                  left: "700px",
+                  top: "200px",
+                  transform: "scale(1.5)",
+                }}
+              >
+                <strong className="purple">Guoyao Li</strong>
+              </h1>
+            </Col>
+          </Row>
+          <Row>
 
-      </Container>
-    </Container>
-        
-      <Container fluid className="home-about-section" id="about">      
-        <Container className="home-about-social">
-          <ul className="home-about-social-links">
-            <li className="social-icons">
-              <a
-                href="https://github.com/guoyaol/"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour  home-social-icons"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/guoyaoli/"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour  home-social-icons"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/gylhimself/"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour home-social-icons"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-          </ul>
+            <Aboutcard />
+            <div className="home-about-social">
+              <ul className="home-about-social-links">
+                <li className="social-icons">
+                  <a
+                    href="https://github.com/guoyaol/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.linkedin.com/in/guoyaoli/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour  home-social-icons"
+                  >
+                    <FaLinkedinIn />
+                  </a>
+                </li>
+                <li className="social-icons">
+                  <a
+                    href="https://www.instagram.com/gylhimself/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="icon-colour home-social-icons"
+                  >
+                    <AiFillInstagram />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </Row>
         </Container>
-      </Container >
-
-
+      </Container>
     </section>
   );
 }
